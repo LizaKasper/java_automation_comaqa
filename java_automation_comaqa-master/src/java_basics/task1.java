14997c5229ef941b8f6ed30c778fcb011391124e
Liza Kaspiarovich <75796559+LizaKasper@users.noreply.github.com>
2021-12-09 21:33:46 +0300

    Delete java_automation_comaqa-master directory

--- java_automation_comaqa-master/src/java_basics/task1.java
+++
@@ -1,9 +0,0 @@
-package java_basics;
-
-
-public class task1 {
-    //Написать программу, выводящую информацию о себе.
-    public static void main(String[] args) {
-        System.out.println("Информация обо мне");
-    }
-}
